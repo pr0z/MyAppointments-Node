ac0960fb717812fb850798dae3e1b24a089e2cc8
ThundeR78 <julienwetstein@gmail.com>
2014-05-06 18:24:11 +0200

    return array users works

--- routes/users.js
+++ routes/users.js
@@ -21,11 +21,52 @@ router.route('/InsertNewUser/')
          	USR_PHONE : "0609880736",
          	USR_CREATION_DATE : "06/05/2014"
 		});
-  
-   		res.send('GET Users');	
-   	})
-   	.post(function(req, res, next) {
-   		res.send('POST Users');
-   	});
+ 
+   		database.collection('USERS').insert(users, function(err, cursor) {});
 
-   module.exports = router;
+		res.send('Utilisateur enregistré');	
+	})
+	.post(function(req, res, next) {
+		res.send('POST Users');
+	});
+
+router.route('/GetUserByMail/:userMail')
+	.get(function(req, res, next) {
+		// res.send("PARAM "+ req.params.userMail);
+
+		 database.collection('USERS', function(err, collection) {
+	        collection.find().toArray(function(err, items) {
+	            res.send(items);
+	        });
+	    });
+
+	     // database.collection('USERS').find(
+	     // {
+	     //    // USERS_EMAIL : req.params.userMail
+	     // },
+	     // {
+	     //    //  _id : 0,
+	     //    // USR_EMAIL : 1
+	     // }, 
+	     // function(err, item) {
+	     // 	if (err) {
+	     // 		res.send("ERROR "+err);
+	     // 	} else {
+	     // 		// res.send("ITEM "+ item);
+	     // 		res.json(item);
+	     // 	}
+	     //    // var next = function() {
+	     //    //    cursor.nextObject(function(err, item) {
+	     //    //       if (err || !item) {
+	     //    //          return;
+	     //    //       }
+	              
+	     //    //       res.send(item);
+	     //    //       next();
+	     //    //    })
+	     //    // }
+	     //    // next();
+	     // });
+	})
+
+module.exports = router;
