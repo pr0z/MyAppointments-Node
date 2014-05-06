cbac23aa2ce94013a50c84390a538931112426a4
ThundeR78 <julienwetstein@gmail.com>
2014-05-06 10:42:35 +0200

    Use Express

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+// server.js (Express 4.0)
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var app            = express();
+
+app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 					// log every request to the console
+app.use(bodyParser()); 						// pull information from html in POST
+app.use(methodOverride()); 					// simulate DELETE and PUT
+
 var http = require('http')
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
+
+app(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Azure !\n');
 }).listen(port);
+
+console.log('Magic happens on port '+port); 	
