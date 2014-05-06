85643c1552f2d1f21635799a0a39ee377da6286e
pr0z <roman.leichnig@gmail.com>
2014-05-06 17:48:21 +0200

    controller user

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,59 @@
-   var express = require('express');
-   var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-   /* GET users listing. */
-   router.route('/:userId')
-   	.get(function(req, res, next) {
+router.route('/')
+   .get(function(req, res, next){
+      res.send("Get Users");
+   });
 
-   		users = [];
-   		users.push({
-            USR_ID = 1,
-            USR_FIRSTNAME = "Roman",
-            USR_LASTNAME = "Leichnig",
-            USR_BIRTHDATE =  "14/05/1991",
-            USR_PASSWORD = "toto",
-            USR_PHONE = "0609880736",
-            USR_CREATION_DATE = "06/05/2014"
-   		});
-     
+/* GET users listing. */
+router.route('/InserNewUser/')
+	.get(function(req, res, next) {
 
-      	database.collection('USERS').insert(users, function(err, cursor) {});
+		users = [];
+		users.push({
+         USR_ID : 1,
+         USR_FIRSTNAME : "Roman",
+         USR_LASTNAME : "Leichnig",
+         USR_BIRTHDATE :  "14/05/1991",
+         USR_EMAIL : "roman.leichnig@gmail.com",
+         USR_PASSWORD : "toto",
+         USR_PHONE : "0609880736",
+         USR_CREATION_DATE : "06/05/2014"
+		});
+  
 
-   		res.send('utilisateur enregistré');	
-   	})
-   	.post(function(req, res, next) {
-   		res.send('POST Users');
-   	});
+   	database.collection('USERS').insert(users, function(err, cursor) {});
 
-   module.exports = router;
+		res.send('utilisateur enregistré');	
+	})
+	.post(function(req, res, next) {
+		res.send('POST Users');
+	});
+
+   router.route('/GetUserByMail/:userMail')
+      .get(function(req, res, next) {
+         database.collection('USERS').find(
+         {
+            'USERS_EMAIL' : req.params.userMail
+         },
+         {
+             _id : 0,
+            USR_EMAIL : 1
+         }, 
+         function(err, cursor) {
+            var next = function() {
+               cursor.nextObject(function(err, item) {
+                  if (err || !item) {
+                     return;
+                  }
+                  
+                  res.json(item);
+                  next();
+               })
+            }
+            next();
+         });
+   })
+
+module.exports = router;
