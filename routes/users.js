5583277f9f00e9578f261aaa7242daa9e56f60d4
ThundeR78 <julienwetstein@gmail.com>
2014-05-07 10:36:05 +0200

    RegisterUser to test

--- routes/users.js
+++ routes/users.js
@@ -6,29 +6,37 @@ router.route('/')
       res.send("Get Users");
    });
 
-/* GET users listing. */
-router.route('/InsertNewUser/')
+router.route('/RegisterUser')
 	.get(function(req, res, next) {
-
 		users = [];
-		users.push({
-         	Id : 1,
-         	FirstName : "Roman",
-         	LastName : "Leichnig",
-         	BirthDate :  "14/05/1991",
-         	Email : "roman.leichnig@gmail.com",
-         	Password : "toto",
-         	Phone : "0609880736",
-         	CreationDate : "06/05/2014"
-		});
- 
-   		database.collection('USERS').insert(users, function(err, cursor) {});
 
-		res.send('Utilisateur enregistré');	
+		user = createUser(1,"Roman","Leichnig","14/05/1991","roman.leichnig@gmail.com","toto","0609880736","06/05/2014");
+		users.push(user);
+ 
+   		database.collection('USERS').insert(user);
+
+		res.send('Utilisateur enregistré');
 	})
 	.post(function(req, res, next) {
-		res.send('POST Users');
+		user = createUser(req.body.Id,req.body.FirstName,req.body.LastName,req.body.BirthDate,req.body.Email,req.body.Password,req.body.Phone,req.body.CreationDate);
+
+		database.collection('USERS').insert(user);
+
+		res.send('Utilisateur enregistré');	
 	});
+
+	function createUser(id, firstname, lastname, birthdate, email, password, phone, creationdate) {
+		return {
+			Id : id,
+         	FirstName : firstname,
+         	LastName : lastname,
+         	BirthDate :  birthdate,
+         	Email : email,
+         	Password : password,
+         	Phone : phone,
+         	CreationDate : creationdate
+		};
+	}
 
 	router.route('/GetUserByMail/:userMail')
 		.get(function(req, res, next) {
@@ -51,11 +59,6 @@ router.route('/InsertNewUser/')
 	     	}
 	        
 	     });
-	})
-
-router.route('/RegisterUser')
-	.post(function(req, res, next) {
-		 
 	})
 
 router.route('/UpdateUser')
