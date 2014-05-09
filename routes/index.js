0ed63cd5d6c618fdd9db6a1eaffadfe940a79f86
pr0z <roman.leichnig@gmail.com>
2014-05-09 12:51:11 +0200

    reséparation dans fichier séparés

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,6 @@
-var express = require('express');
-var app = express();
+module.exports = function(app){
 
-/* GET home page. */
-app.get('/', function(req, res, next) {
-  		res.render('index', { title: 'Express' });
-  	});
- //  	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	// res.end('Hello Azure with Express !\n');
-
-
-app.get('/Task', function(req, res, next) {
-		res.render('index', { toto : 1 });
-	})
-app.post('/Task/', function(req, res, next) {
-		res.send('POST Tasks');
-	});
-
-module.exports = app;
+    app.get('/', function(req, res){
+        res.render('index');
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,98 +1,41 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var router = express.Router();
+module.exports = function(app){
 
-router.route('/')
-   .get(function(req, res, next){
-      res.send("Get Users");
-   });
-
-/* GET users listing. */
-router.route('/InsertNewUser/')
-	.get(function(req, res, next) {
-
-		users = [];
-		users.push({
-         	Id : 1,
-         	FirstName : "leichnig",
-         	LastName : "roman",
-         	BirthDate :  new Date(1991, 05, 14),
-         	Email : "roman.leichnig@gmail.com",
-         	Password : "toto",
-         	Phone : "0609880736",
-         	CreationDate : new Date()
-		});
-
-		console.log(database);
-
-   		database.collection('USERS').insert(users, function(err, cursor) {
-   			if(err)
-   				console.log(err);
-   		});
-
-		res.send('Utilisateur enregistré');	
-	})
-	.post(function(req, res, next) {
-		res.send('POST Users');
-	});
-
-	router.route('/GetUserByMail/:userMail')
-		.get(function(req, res, next) {
-		 // res.send("PARAM "+ req.params.userMail);
-
-	  //   database.collection('USERS').find().limit(1);
-
-	     database.collection('USERS').findOne(
-	     {
-	        Email : req.params.userMail
-	     },
-	     {
-	         _id : 0
-	     }
-	     , function(err, item) {
-	     	if (err) {
-	     		res.send("ERROR "+err);
-	     	} else {
-	     		res.json(item);
-	     	}	        
-	     });
+	app.get('/Users/GetUserByMail/:userMail', function(req, res, next) {
+	  database.collection('USERS').findOne(
+	  {
+	    Email : req.params.userMail
+	  },
+	  {
+	    _id : 0
+	  }
+	  , function(err, item) {
+	      if (err) {
+	        res.send("ERROR "+err);
+	    } else {
+	      console.log("get user by mail");
+	      res.json(item);
+	    }        
+	  });
 	})
 
-router.route('/RegisterUser')
-	.all(function(req, res, next) {
-		console.log(next);
-		res.send("toto");
-	})
+	app.get('/Users/InsertNewUser/', function(req, res, next) {
+        users = [];
+        users.push({
+            Id : 1,
+            FirstName : "leichnig",
+            LastName : "roman",
+            BirthDate :  new Date(1991, 05, 14),
+            Email : "roman.leichnig@gmail.com",
+            Password : "toto",
+            Phone : "0609880736",
+            CreationDate : new Date()
+        });
 
-router.route('/UpdateUser/:id')
-	.get(function(req, res, next) {
-	    database.collection('USERS').update(
-	    	{
-	    		Id : +req.params.id
-	    	},  
-	    	{
-	    		$set:{
-	    			FirstName : "Michel"
-	    		}
-	    	}, 
-	    	{ w: 0 });
+        database.collection('USERS').insert(users, function(err, cursor) {
+            if(err)
+                console.log(err);
+        });
 
-	    res.send('Utilisateur qui a pour ID '+req.params.id +' modifié');
-	})
-
-router.route('/DeleteUser/:id')
-	.get(function(req, res, next) {
-		 database.collection('USERS').remove(
-         {
-            Id : +req.params.id
-         }
-         , function(err, item) {
-         if (err) {
-            res.send("ERROR "+err);
-         } else {
-            res.json(item);
-         }
-       });  
-	})
-
-module.exports = router;
+        res.send('Utilisateur enregistré'); 
+ 	})
+}
