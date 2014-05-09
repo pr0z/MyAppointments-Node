50074ce5ac2e51f875a82e6f38f730cbe5fbd7a8
pr0z <roman.leichnig@gmail.com>
2014-05-09 10:03:01 +0200

    fichier user ben

--- routes/users.js
+++ routes/users.js
@@ -1,77 +1,98 @@
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var router = express.Router();
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
 
-// router.route('/')
-//    .get(function(req, res, next){
-//       res.send("Get Users");
-//    });
+router.route('/')
+   .get(function(req, res, next){
+      res.send("Get Users");
+   });
 
-// /* GET users listing. */
-// router.route('/InsertNewUser/')
-// 	.get(function(req, res, next) {
+/* GET users listing. */
+router.route('/InsertNewUser/')
+	.get(function(req, res, next) {
 
-// 		users = [];
-// 		users.push({
-//          	Id : 1,
-//          	FirstName : "Roman",
-//          	LastName : "Leichnig",
-//          	BirthDate :  "14/05/1991",
-//          	Email : "roman.leichnig@gmail.com",
-//          	Password : "toto",
-//          	Phone : "0609880736",
-//          	CreationDate : "06/05/2014"
-// 		});
+		users = [];
+		users.push({
+         	Id : 1,
+         	FirstName : "leichnig",
+         	LastName : "roman",
+         	BirthDate :  new Date(1991, 05, 14),
+         	Email : "roman.leichnig@gmail.com",
+         	Password : "toto",
+         	Phone : "0609880736",
+         	CreationDate : new Date()
+		});
 
-// 		console.log(database);
+		console.log(database);
 
-//    		database.collection('USERS').insert(users, function(err, cursor) {
-//    			if(err)
-//    				console.log(err);
-//    		});
+   		database.collection('USERS').insert(users, function(err, cursor) {
+   			if(err)
+   				console.log(err);
+   		});
 
-// 		res.send('Utilisateur enregistré');	
-// 	})
-// 	.post(function(req, res, next) {
-// 		res.send('POST Users');
-// 	});
+		res.send('Utilisateur enregistré');	
+	})
+	.post(function(req, res, next) {
+		res.send('POST Users');
+	});
 
-// 	router.route('/GetUserByMail/:userMail')
-// 		.get(function(req, res, next) {
-// 		 // res.send("PARAM "+ req.params.userMail);
+	router.route('/GetUserByMail/:userMail')
+		.get(function(req, res, next) {
+		 // res.send("PARAM "+ req.params.userMail);
 
-// 	  //   database.collection('USERS').find().limit(1);
+	  //   database.collection('USERS').find().limit(1);
 
-// 	     database.collection('USERS').findOne(
-// 	     {
-// 	        Email : req.params.userMail
-// 	     },
-// 	     {
-// 	         _id : 0
-// 	     }
-// 	     , function(err, item) {
-// 	     	if (err) {
-// 	     		res.send("ERROR "+err);
-// 	     	} else {
-// 	     		res.json(item);
-// 	     	}
-	        
-// 	     });
-// 	})
+	     database.collection('USERS').findOne(
+	     {
+	        Email : req.params.userMail
+	     },
+	     {
+	         _id : 0
+	     }
+	     , function(err, item) {
+	     	if (err) {
+	     		res.send("ERROR "+err);
+	     	} else {
+	     		res.json(item);
+	     	}	        
+	     });
+	})
 
-// router.route('/RegisterUser')
-// 	.all(function(req, res, next) {
-// 		console.log(next);
-// 		res.send("toto");
-// 	})
+router.route('/RegisterUser')
+	.all(function(req, res, next) {
+		console.log(next);
+		res.send("toto");
+	})
 
-// router.route('/UpdateUser')
-// 	.get(function(req, res, next) {
-// 	})
+router.route('/UpdateUser/:id')
+	.get(function(req, res, next) {
+	    database.collection('USERS').update(
+	    	{
+	    		Id : +req.params.id
+	    	},  
+	    	{
+	    		$set:{
+	    			FirstName : "Michel"
+	    		}
+	    	}, 
+	    	{ w: 0 });
 
-// router.route('/DeleteUser')
-// 	.post(function(req, res, next) {
-		 
-// 	})
+	    res.send('Utilisateur qui a pour ID '+req.params.id +' modifié');
+	})
 
-// module.exports = router;
+router.route('/DeleteUser/:id')
+	.get(function(req, res, next) {
+		 database.collection('USERS').remove(
+         {
+            Id : +req.params.id
+         }
+         , function(err, item) {
+         if (err) {
+            res.send("ERROR "+err);
+         } else {
+            res.json(item);
+         }
+       });  
+	})
+
+module.exports = router;
